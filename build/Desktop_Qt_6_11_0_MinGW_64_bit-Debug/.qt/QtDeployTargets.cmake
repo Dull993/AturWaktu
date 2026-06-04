c28c2822631c9fdc78b513f3ca1baa46a4d75488
ToraEleazarTarigan-251401037 <toratarigan7d@gmail.com>
2026-06-05 01:14:06 +0700

    Perbaiki codingan

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++ build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -1,3 +1,3 @@
-set(__QT_DEPLOY_TARGET_AturWaktu_FILE D:/AturWaktu/AturWaktu/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/AturWaktu.exe)
+set(__QT_DEPLOY_TARGET_AturWaktu_FILE C:/Users/torae/Downloads/AturWaktuV0.1-20260604T051701Z-3-001/AturWaktuV0.1/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/AturWaktu.exe)
 set(__QT_DEPLOY_TARGET_AturWaktu_TYPE EXECUTABLE)
 set(__QT_DEPLOY_TARGET_AturWaktu_RUNTIME_DLLS D:/qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;D:/qt/6.11.0/mingw_64/bin/Qt6Gui.dll;D:/qt/6.11.0/mingw_64/bin/Qt6Core.dll)
